471017bb03aaee4271635b58cceab6a9f3aa026e
Jose Pereira <hefdex@gmail.com>
2021-12-24 22:47:26 +0100

    Created example

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
-type Block struct {
-	Data     []byte
-	Hash     []byte
-	PrevHash []byte
-}
+import (
+	"fmt"
 
-type BlockChain struct {
-	blocks []*Block
+	"github.com/hetfdex/blockchain-go/orchestrator"
+)
+
+func main() {
+	db, wrapper, err := orchestrator.InitDb()
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	bc, err := orchestrator.InitBlockchain(wrapper)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 5; i++ {
+		err = orchestrator.AddBlock(bc, fmt.Sprintf("%d", i))
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err = orchestrator.PrintBlocks(bc)
+
+	if err != nil {
+		panic(err)
+	}
 }
