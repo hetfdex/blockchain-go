f0790413294222b8c5b7da162dd7e0c3a42a8b0a
Jose Pereira <hefdex@gmail.com>
2021-12-24 22:46:37 +0100

    Implemented wrapper around badger and mock

---
+++ badgerwrapper/badgerwrapper.go
@@ -0,0 +1,66 @@
+package badgerwrapper
+
+import (
+	"errors"
+
+	"github.com/dgraph-io/badger/v3"
+)
+
+var (
+	ErrBlockNotFound = errors.New("block not found")
+)
+
+type BadgerWrapper interface {
+	Set(key []byte, value []byte) error
+	Get(key []byte) ([]byte, error)
+}
+
+type badgerWrapper struct {
+	db *badger.DB
+}
+
+func New(db *badger.DB) BadgerWrapper {
+	return &badgerWrapper{
+		db: db,
+	}
+}
+
+func (w *badgerWrapper) Set(key []byte, value []byte) error {
+	err := w.db.Update(func(txn *badger.Txn) error {
+		return txn.Set(key, value)
+	})
+	return err
+}
+
+func (w *badgerWrapper) Get(key []byte) ([]byte, error) {
+	var res []byte
+
+	err := w.db.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(key)
+
+		if err != nil {
+			return err
+		}
+
+		res, err = item.ValueCopy(nil)
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, handleErrKeyNotFound(err)
+	}
+
+	return res, nil
+}
+
+func handleErrKeyNotFound(err error) error {
+	if err == badger.ErrKeyNotFound {
+		return ErrBlockNotFound
+	}
+	return err
+}
