80bd6b06bccae58933f37d1337a0c91afb1fc5d9
Jose Pereira <hefdex@gmail.com>
2021-12-25 10:01:36 +0100

    Removed pointers

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -13,23 +13,23 @@ const (
 )
 
 type Blockchain interface {
-	Set(block *block.Block) error
-	Get(hash []byte) (*block.Block, error)
-	GetLatest() (*block.Block, error)
+	Set(block block.Block) error
+	Get(hash []byte) (block.Block, error)
+	GetLatest() (block.Block, error)
 }
 
 type blockchain struct {
 	wrapper     badgerwrapper.BadgerWrapper
-	latestBlock *block.Block
+	latestBlock block.Block
 }
 
-func New(wrapper badgerwrapper.BadgerWrapper) Blockchain {
+func New(wrapper badgerwrapper.BadgerWrapper) *blockchain {
 	return &blockchain{
 		wrapper: wrapper,
 	}
 }
 
-func (bc *blockchain) Set(block *block.Block) error {
+func (bc *blockchain) Set(block block.Block) error {
 	jsonBlock, err := json.Marshal(block)
 
 	if err != nil {
@@ -52,38 +52,38 @@ func (bc *blockchain) Set(block *block.Block) error {
 	return nil
 }
 
-func (bc *blockchain) Get(hash []byte) (*block.Block, error) {
+func (bc *blockchain) Get(hash []byte) (block.Block, error) {
+	var b block.Block
+
 	jsonBlock, err := bc.wrapper.Get(hash)
 
 	if err != nil {
-		return nil, err
+		return b, err
 	}
-
-	var b block.Block
 
 	err = json.Unmarshal(jsonBlock, &b)
 
 	if err != nil {
-		return nil, err
+		return b, err
 	}
-	return &b, nil
+	return b, nil
 }
 
-func (bc *blockchain) GetLatest() (*block.Block, error) {
-	if bc.latestBlock != nil {
+func (bc *blockchain) GetLatest() (block.Block, error) {
+	if bc.latestBlock.Hash != nil && len(bc.latestBlock.Hash) > 0 {
 		return bc.latestBlock, nil
 	}
 
 	latestBlockHash, err := bc.wrapper.Get([]byte(latestBlockHashKey))
 
 	if err != nil {
-		return nil, err
+		return bc.latestBlock, err
 	}
 
 	latestBlock, err := bc.Get(latestBlockHash)
 
 	if err != nil {
-		return nil, err
+		return bc.latestBlock, err
 	}
 	bc.latestBlock = latestBlock
 
