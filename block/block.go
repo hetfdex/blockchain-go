1f2998c2114e41c101b230a56b6d88881ed00b9c
Jose Pereira <hefdex@gmail.com>
2021-12-25 13:02:29 +0100

    Integrated pow with block

--- block/block.go
+++ block/block.go
@@ -1,25 +1,102 @@
 package block
 
-const (
-	genesisBlockData = "genesis_block_data"
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"math"
+	"math/big"
 )
 
+const (
+	genesisBlockData = "genesis_block_data"
+	difficulty       = 10
+)
+
+type ProofOfWork interface {
+	Validate() bool
+}
+
 type Block struct {
-	Data     []byte `json:"data"`
-	PrevHash []byte `json:"prev_hash"`
-	Hash     []byte `json:"hash"`
-	Nonce    uint64 `json:"nonce"`
+	Data     []byte   `json:"data"`
+	PrevHash []byte   `json:"prev_hash"`
+	Hash     []byte   `json:"hash"`
+	Nonce    uint64   `json:"nonce"`
+	Target   *big.Int `json:"target"`
 }
 
 func New(data string, prevHash []byte) Block {
-	return Block{
+	target := big.NewInt(1)
+
+	target.Lsh(target, uint(256-difficulty))
+
+	b := Block{
 		Data:     []byte(data),
 		PrevHash: prevHash,
 		Hash:     []byte{},
 		Nonce:    0,
+		Target:   target,
 	}
+
+	b.prove()
+
+	return b
 }
 
 func NewGenesis() Block {
 	return New(genesisBlockData, []byte{})
+}
+
+func (b *Block) Validate() bool {
+	var intHash big.Int
+
+	data := b.init(b.Nonce)
+
+	hash := sha256.Sum256(data)
+
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(b.Target) == -1
+}
+
+func (b *Block) prove() {
+	var intHash big.Int
+	var hash [32]byte
+
+	var nonce uint64
+
+	for nonce < math.MaxInt64 {
+		data := b.init(nonce)
+
+		hash = sha256.Sum256(data)
+
+		intHash.SetBytes(hash[:])
+
+		if intHash.Cmp(b.Target) == -1 {
+			break
+		}
+		nonce++
+	}
+	b.Nonce = nonce
+	b.Hash = hash[:]
+}
+
+func (b *Block) init(nonce uint64) []byte {
+	return bytes.Join(
+		[][]byte{
+			b.PrevHash,
+			b.Data,
+			toHex(nonce),
+			toHex(difficulty),
+		},
+		[]byte{},
+	)
+}
+
+func toHex(i uint64) []byte {
+	buffer := new(bytes.Buffer)
+
+	_ = binary.Write(buffer, binary.BigEndian, i)
+
+	return buffer.Bytes()
 }
--- block/block_test.go
+++ block/block_test.go
@@ -6,16 +6,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewBlock(t *testing.T) {
-	data := "test_data"
-	prevHash := []byte("test_prev_hash")
+const (
+	data = "test_data"
+)
 
+var (
+	prevHash = []byte("test_prev_hash")
+)
+
+func TestNewBlock(t *testing.T) {
 	b := New(data, prevHash)
 
 	assert.Equal(t, []byte(data), b.Data)
 	assert.Equal(t, prevHash, b.PrevHash)
-	assert.Equal(t, uint64(0), b.Nonce)
-	assert.Equal(t, 0, len(b.Hash))
+	assert.Equal(t, 32, len(b.Hash))
+	assert.True(t, b.Nonce != 0)
+	assert.Equal(t, 31, len(b.Target.Bytes()))
 }
 
 func TestNewGenesisBlock(t *testing.T) {
@@ -23,6 +29,21 @@ func TestNewGenesisBlock(t *testing.T) {
 
 	assert.Equal(t, []byte(genesisBlockData), b.Data)
 	assert.Equal(t, []byte{}, b.PrevHash)
-	assert.Equal(t, uint64(0), b.Nonce)
-	assert.Equal(t, 0, len(b.Hash))
+	assert.Equal(t, 32, len(b.Hash))
+	assert.True(t, b.Nonce != 0)
+	assert.Equal(t, 31, len(b.Target.Bytes()))
+}
+
+func TestValidate_False(t *testing.T) {
+	b := New(data, prevHash)
+
+	b.Nonce = 666
+
+	assert.False(t, b.Validate())
+}
+
+func TestValidate_True(t *testing.T) {
+	b := New(data, prevHash)
+
+	assert.True(t, b.Validate())
 }
